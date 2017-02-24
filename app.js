8b3b5b91357cc1ec678abdf6173fc6aa7cb3968b
Heda <450307@CTS.COM>
2017-02-24 18:49:58 +0530

    app.js - new MS ID and PSWRD added

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ var builder = require('botbuilder');
 
 // Setup Restify Server
 var server = restify.createServer();
+app.set('port', process.env.PORT)
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: '97e7527a-27d1-4125-9d45-327549d13d98',
-    appPassword: 'xFGGhnMjQSM3hdmeJ9ZY9S8'
+    appId: 'dd891a7a-0888-4798-b396-dc1cd74f1314',
+    appPassword: 'E2zqFoxhFUgBDt8HrV4MsmB'
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
